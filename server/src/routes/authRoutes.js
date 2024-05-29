1305ee2b9a8134889a09e5d54d5f86a1f03c163a
KnightSS-10 <kushagrassisodia@gmail.com>
2024-05-30 00:12:11 +0530

    add user input validation

--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -1,11 +1,26 @@
 import express from "express";
+import Joi from "joi";
+import ExpressValidator from "express-joi-validation";
 
 import { postLogin, postRegister } from "../controllers/controllers.js";
 
+const validator = ExpressValidator.createValidator({});
+
+const registerSchema = Joi.object({
+    username : Joi.string().min(3).max(30).required(),
+    password : Joi.string().min(3).max(30).required(),
+    email : Joi.string().email().required()
+});
+
+const loginSchema = Joi.object({
+    password : Joi.string().min(3).max(30).required(),
+    email : Joi.string().email().required()
+});
+
 const router = express.Router();
 
-router.post("/register", postRegister);
+router.post("/register", validator.body(registerSchema), postRegister);
 
-router.post("/login", postLogin);
+router.post("/login", validator.body(loginSchema), postLogin);
 
 export default router;
