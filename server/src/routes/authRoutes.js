56934079daa9e7763bfe441cbc02457d37a30ab0
KnightSS-10 <kushagrassisodia@gmail.com>
2024-05-30 00:57:21 +0530

    add conditional validation in joi

--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -9,9 +9,11 @@ const validator = ExpressValidator.createValidator({});
 const registerSchema = Joi.object({
     username : Joi.string().min(3).max(30).required(),
     password : Joi.string().min(3).max(30).required(),
-    email : Joi.string().email().required()
+    preference_method : Joi.string().required().valid('email', 'phone_no'),
+    email : Joi.when('preference_method', { is: 'email', then: Joi.string().email().required() }),
+    phone_no : Joi.when('preference_method', { is: 'phone_no', then: Joi.string().required() })
 });
-
+//apply method of preference between contact no. and email
 const loginSchema = Joi.object({
     password : Joi.string().min(3).max(30).required(),
     email : Joi.string().email().required()
